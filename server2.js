27eeee7f306e5950ccdedc43a8b5f525e0681006
Brandi Murray <pritchettb14@gmail.com>
2021-04-20 13:44:42 -0400

    more routes

--- server2.js
+++ server2.js
@@ -41,7 +41,7 @@ const reservations = [
     }
 ]
 
-
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
-
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
