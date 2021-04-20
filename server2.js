b9528a0dd749d249fc88dea9c63b85a0d8f27732
Brandi Murray <pritchettb14@gmail.com>
2021-04-20 14:03:17 -0400

    added route for getting all reservation data

--- server2.js
+++ server2.js
@@ -44,4 +44,9 @@ const reservations = [
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
 app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
 app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
+
+app.get('/api/tables', (req, res) => res.json(reservations));
+
+
+
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
