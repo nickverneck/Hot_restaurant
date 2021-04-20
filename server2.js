27826dec620deaf407cb661851b461c34ffb1bd0
Brandi Murray <pritchettb14@gmail.com>
2021-04-20 13:43:01 -0400

    route 1

--- server2.js
+++ server2.js
@@ -41,4 +41,7 @@ const reservations = [
     }
 ]
 
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
